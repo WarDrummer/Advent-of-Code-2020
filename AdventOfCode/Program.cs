e92d7551d899f63b1f82a2bef539cb900292ccc7
Jason Pike <jason@Jasons-MacBook-Pro.local>
2020-12-06 12:18:44 -0600

    templates for previous years

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -4,6 +4,11 @@
     {
         static void Main()
         {
+            // new Year2015Executor().Execute();
+            // new Year2016Executor().Execute();
+            // new Year2017Executor().Execute();
+            // new Year2018Executor().Execute();
+            // new Year2019Executor().Execute();
             new Year2020Executor().Execute();
         }
     }
